93c1eea197c3c9d0d94a14955c8d498c05bef607
Mauricio Torres <mauricio.torres.ma@outlook.com>
2023-07-12 01:13:52 -0500

    New version of vmware hostmodules

--- RecompileVMWare.sh
+++ RecompileVMWare.sh
@@ -1,11 +1,13 @@
-vmware_user="XXXXXX"
-vmware_path="/home/$vmware_user/vmware/"
-vmware_version=$(vmware --version|awk '{print $3}')
-kernel_version=$(uname -r|awk -F"." '{print $1"."$2}')
-host_module="vmware-host-modules-w"$vmware_version"-k"$kernel_version
-zip_module="w"$vmware_version"-k"$kernel_version".zip"
-url="https://github.com/mkubecek/vmware-host-modules/archive"
 
+vmware_user="mtorres"
+vmware_path="/home/$vmware_user/VM/vmware/"
+vmware_version=$(vmware --version|awk '{print $3}')
+# kernel_version=$(uname -r|awk -F"." '{print $1"."$2}')
+host_module="vmware-host-modules-w"$vmware_version
+zip_module="w"$vmware_version".zip"
+url="https://github.com/mkubecek/vmware-host-modules/archive/refs/tags"
+
+echo URL:$url/$zip_module
 
 cd $vmware_path
 rm -rf $host_module
@@ -23,9 +25,19 @@ make
 make install
 
 cd $vmware_path
-/usr/src/kernels/$(uname -r)/scripts/sign-file sha256 ./MOK.priv ./MOK.der $(modinfo -n vmmon)
-/usr/src/kernels/$(uname -r)/scripts/sign-file sha256 ./MOK.priv ./MOK.der $(modinfo -n vmnet)
+#/usr/src/kernels/$(uname -r)/scripts/sign-file sha256 ./MOK.priv ./MOK.der $(modinfo -n vmmon)
+#/usr/src/kernels/$(uname -r)/scripts/sign-file sha256 ./MOK.priv ./MOK.der $(modinfo -n vmnet)
 
 rm -rf $host_module
 
 systemctl restart vmware
+
+#$wget https://github.com/mkubecek/vmware-host-modules/archive/workstation-17.0.0.tar.gz
+# sudo rm -rf vmware-host-modules-workstation-17.0.0
+# tar -xzf workstation-17.0.0.tar.gz
+# cd vmware-host-modules-workstation-17.0.0
+# make
+# sudo make install
+# sudo systemctl restart vmware
+# exit 0
+#=============================================================
